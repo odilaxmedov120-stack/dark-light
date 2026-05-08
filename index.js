f82f13826fc0801766f1188753e3bb1d2a4eabca
odilaxmedov120-stack <odilaxmedov@gmail.com>
2026-05-08 16:58:34 +0500

    add project:

---
+++ index.js
@@ -0,0 +1,8 @@
+const darkMode = document.querySelector(".dark-mode")
+const lightMode = document.querySelector(".light-mode")
+
+if(localStorage.getItem("mode") === "dark") {
+    document.body.classList.add("dark")
+} else {
+    document.body.classList
+}
